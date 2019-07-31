77662acef458101504b00e560fc2bc1a9848b56f
Toru Oshima <selfmadetool0213@icloud.com>
2019-07-31 22:38:40 +0900

    create sending blog function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,22 @@
 class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.includes(:user).order("id DESC")
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    if Blog.create(blog_params)
+      redirect_to root_path
+      # アラートを表示
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :text).merge(user_id: current_user.id)
+  end
 end
