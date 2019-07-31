24025f93fbd99724794a40a2aa5965bfa7f9d572
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-01 02:05:22 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "blogs#index"
-  resources :blogs, only: [:index, :new, :create, :show]
+  resources :blogs, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: [:edit, :update]
 end
