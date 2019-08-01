77dee7dc9dd92fad81cf042c76f022e475aca91b
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-02 01:51:02 +0900

    create ajax done and fail function

--- app/assets/javascripts/menu-bar.js
+++ app/assets/javascripts/menu-bar.js
@@ -1,19 +1,42 @@
 $(document).on('turbolinks:load', function() {
+  function appendUser(user) {
+    var userHTML = `<div class="blog-user">
+                      <p class="blog-user__nickname">${user.nickname}</p>
+                    </div>`
 
+    $('.main').append(userHTML);
+  }
+
+  function appendErrorMessage(message) {
+    var messageHTML = `<div class="blog-user">
+                        <p class="blog-user__nickname">${message}</p>
+                      </div>`
+    $('.main').append(messageHTML);
+  }
   $('#nickname-form').on('keyup', function() {
     var input = $(this).val();
+
     $.ajax({
       type: 'GET',
       url: '/users',
       data: {keyword: input},
       dataType: 'json'
     })
-
-
-
-
-
-
+    .done(function(users) {
+      $('.main').empty();
+      if (input.length !== 0) {
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            appendUser(user);
+          })
+        } else {
+          appendErrorMessage("一致するユーザーが見つかりません");
+        }
+      }
+    })
+    .fail(function() {
+      alert('ユーザーの検索に失敗しました')
+    })
   });
 
 
