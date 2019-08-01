52f42791941232188ff143bb62519c7c40034bff
Toru Oshima <selfmadetool0213@icloud.com>
2019-08-02 00:45:39 +0900

    Update routing and controller

--- app/assets/javascripts/menu-bar.js
+++ app/assets/javascripts/menu-bar.js
@@ -1,11 +1,28 @@
 $(document).on('turbolinks:load', function() {
+
+  $('#nickname-form').on('keyup', function() {
+    var input = $(this).val();
+    
+
+
+
+
+
+    
+  });
+
+
+
+
+
+
   $('.fa-bars').on('click', function() {
     $('.header__links').css('display', 'block');
-    $('.header__icon').css('display', 'none')
+    $('.header__icon').css('display', 'none');
   })
 
   $('.header__links__back').on('click', function() {
     $('.header__links').css('display', 'none');
-    $('.header__icon').css('display', 'block')
+    $('.header__icon').css('display', 'block');
   })
 });
